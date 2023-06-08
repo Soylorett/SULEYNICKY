30235fa43128ca166a7ff5391da3109ae7f64edd
Nicole Loreta <112711690+Soylorett@users.noreply.github.com>
2023-06-07 22:20:50 -0600

    NICOLE YA ACABE Y ME ENOJE

--- js/appLogin.js
+++ js/appLogin.js
@@ -1,10 +1,100 @@
+var sesion=localStorage.getItem("nombre")
+
+const checarSesion=()=>{
+  if(sesion|=null){
+    window.location.href="inicio.html";
+
+  }
+
+}
+
 const registrarUsuario= async()=>{
+    var correo=document.querySelector("#correo").value;
+    var password=document.querySelector("#password").value;
+    var nombre=document.querySelector("#nombre").value;
+    if(correo.trim()==='' ||
+    password.trim()==='' ||
+    nombre.trim()==='' ){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'FALTA LLENAR CAMPOS',
+            footer: 'CRUD CONTACTOS'
+          })
+          return;
+    }
+
+    if(!validarCorreo(correo)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'INTRODUCE UN CORREO ELECTRONICO VALIDO',
+            footer: 'CRUD CONTACTOS'
+          })
+          return
+    }
+    if(!validarPassword(password)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            html: 'INTRODUCE UN PASSWORD VALIDO <br> [Mayusculas, minusculas, numeros y min. 8 Caracteres]',
+            footer: 'CRUD CONTACTOS'
+          })
+          return
+    }
+    if(!validarNombre(nombre)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'INTRODUCE UN NOMBRE VALIDO',
+            footer: 'CRUD CONTACTOS'
+          })
+          return
+    }
+    // INSERTAR A LA BASE DE DATOS
+
+    const datos=new FormData();
+    datos.append("correo",correo);
+    datos.append("password",password);
+    datos.append("nombre",nombre);
+
+
+    var respueta=await fetch("php/usuario/registrarUsuario.php",{
+        method:'POST',
+        body:datos
+    });
+
+
+    var resultado=await respueta.json();
+
+    if(resultado.success==true){
+        Swal.fire({
+            icon: 'success',
+            title: 'EXITO!',
+            text: resultado.mensaje,
+            footer: 'CRUD CONTACTOS'
+          })
+          document.querySelector("#formRegistrar").reset();
+          setTimeout(()=>{
+            window.location.href="index.html";
+          },2000);
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: resultado.mensaje,
+            footer: 'CRUD CONTACTOS'
+          }) 
+    }
+    
+}
+const loginUsuario= async()=>{
   var correo=document.querySelector("#correo").value;
   var password=document.querySelector("#password").value;
-  var nombre=document.querySelector("#nombre").value;
+ 
   if(correo.trim()==='' ||
-  password.trim()==='' ||
-  nombre.trim()==='' ){
+  password.trim()==='' ){
+  
       Swal.fire({
           icon: 'error',
           title: 'ERROR',
@@ -32,24 +122,15 @@ const registrarUsuario= async()=>{
         })
         return
   }
-  if(!validarNombre(nombre)){
-      Swal.fire({
-          icon: 'error',
-          title: 'ERROR',
-          text: 'INTRODUCE UN NOMBRE VALIDO',
-          footer: 'CRUD CONTACTOS'
-        })
-        return
-  }
   // INSERTAR A LA BASE DE DATOS
 
   const datos=new FormData();
   datos.append("correo",correo);
   datos.append("password",password);
-  datos.append("nombre",nombre);
 
 
-  var respueta=await fetch("php/usuario/registrarUsuario.php",{
+
+  var respueta=await fetch("php/usuario/loginUsuario.php",{
       method:'POST',
       body:datos
   });
@@ -65,8 +146,9 @@ const registrarUsuario= async()=>{
           footer: 'CRUD CONTACTOS'
         })
         document.querySelector("#formRegistrar").reset();
+        localStorage.setItem("nombre",resultado.nombre)
         setTimeout(()=>{
-          window.location.href="index.html";
+          window.location.href="inicio.html";
         },2000);
   }else{
       Swal.fire({
@@ -76,75 +158,4 @@ const registrarUsuario= async()=>{
           footer: 'CRUD CONTACTOS'
         }) 
   }
-  
-}
-const loginUsuario= async()=>{
-var correo=document.querySelector("#correo").value;
-var password=document.querySelector("#password").value;
-
-if(correo.trim()==='' ||
-password.trim()==='' ){
-
-    Swal.fire({
-        icon: 'error',
-        title: 'ERROR',
-        text: 'FALTA LLENAR CAMPOS',
-        footer: 'CRUD CONTACTOS'
-      })
-      return;
-}
-
-if(!validarCorreo(correo)){
-    Swal.fire({
-        icon: 'error',
-        title: 'ERROR',
-        text: 'INTRODUCE UN CORREO ELECTRONICO VALIDO',
-        footer: 'CRUD CONTACTOS'
-      })
-      return
-}
-if(!validarPassword(password)){
-    Swal.fire({
-        icon: 'error',
-        title: 'ERROR',
-        html: 'INTRODUCE UN PASSWORD VALIDO <br> [Mayusculas, minusculas, numeros y min. 8 Caracteres]',
-        footer: 'CRUD CONTACTOS'
-      })
-      return
-}
-// INSERTAR A LA BASE DE DATOS
-
-const datos=new FormData();
-datos.append("correo",correo);
-datos.append("password",password);
-
-
-
-var respueta=await fetch("php/usuario/loginUsuario.php",{
-    method:'POST',
-    body:datos
-});
-
-
-var resultado=await respueta.json();
-
-if(resultado.success==true){
-    Swal.fire({
-        icon: 'success',
-        title: 'EXITO!',
-        text: resultado.mensaje,
-        footer: 'CRUD CONTACTOS'
-      })
-      document.querySelector("#formRegistrar").reset();
-      setTimeout(()=>{
-        window.location.href="inicio.html";
-      },2000);
-}else{
-    Swal.fire({
-        icon: 'error',
-        title: 'ERROR',
-        text: resultado.mensaje,
-        footer: 'CRUD CONTACTOS'
-      }) 
-}
 }
